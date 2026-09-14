8f44b30b98e6af38ace626ad4018e451117458cb
samuel yandev <kenteryandev7@gmail.com>
2026-09-14 13:10:18 +0100

    Add validation to display an error if application_open is checked for a past event

--- content/models.py
+++ content/models.py
@@ -315,9 +315,13 @@ class Event(models.Model):
     def clean(self):
         super().clean()
         from django.core.exceptions import ValidationError
+        from django.utils import timezone
 
         if self.start_date and self.end_date and self.end_date < self.start_date:
             raise ValidationError({"end_date": "End date cannot be earlier than start date."})
+
+        if self.application_open and self.start_date and self.start_date < timezone.now():
+            raise ValidationError({"application_open": "The start date has already passed. Applications cannot be open for a past event."})
 
     @property
     def is_upcoming(self):
